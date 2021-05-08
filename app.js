313ef908e25a1cbc0fca05aa53dcf7dea3959f8e
MarcinOsiadacz <marcin.osiadacz@outlook.com>
2021-05-08 09:49:23 +0200

    Bootstrap, JQuery and Express-session included

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+// Import packages
 var express = require('express'),
+    expressSession = require('express-session'),
     path = require('path'),
     bodyParser = require('body-parser'),
     cons = require('consolidate'),
@@ -6,14 +8,18 @@ var express = require('express'),
     pg = require('pg'),
     app = express();
 
+// Db config
 var connectionString = 'postgres://recipebook_user:P@ssw0rd@localhost/recipebook_db';
 
+// Views config
 app.engine('dust', cons.dust);
 app.set('view engine', 'dust');
 app.set('views', __dirname + '/views');
 
+// Define Public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// BodyParser Middleware config
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -21,6 +27,7 @@ app.get('/', function(request, response){
     response.render('index');
 });
 
+// Server config
 app.listen(3000, function(){
     console.log('App running on port 3000');
 });
