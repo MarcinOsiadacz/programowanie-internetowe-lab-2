ea64f15555d21dff9c6052a54044704c5e2bf8af
MarcinOsiadacz <marcin.osiadacz@outlook.com>
2021-05-08 16:46:30 +0200

    Db connection added

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { execPath } = require('process');
+
 // Import packages
 var express = require('express'),
     expressSession = require('express-session'),
@@ -9,7 +11,12 @@ var express = require('express'),
     app = express();
 
 // Db config
-var connectionString = 'postgres://recipebook_user:P@ssw0rd@localhost/recipebook_db';
+const dbClient = new pg.Client({
+    host: 'localhost',
+    database: 'recipebook_db',
+    user: 'recipebook_user',
+    password: 'Passw0rd!'
+});
 
 // Views config
 app.engine('dust', cons.dust);
@@ -24,7 +31,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(request, response){
-    response.render('index');
+    dbClient.connect(err => {
+        if(err) {
+            return console.error('Connection error has occured', err);
+        }
+    });
+    dbClient.query('SELECT * FROM public.recipes', (err, result) => {
+        if(err) {
+            return console.error('Error has occured when running a query', err);
+        }
+        response.render('index', {recipes: result.rows});
+        dbClient.end();
+    });
 });
 
 // Server config
